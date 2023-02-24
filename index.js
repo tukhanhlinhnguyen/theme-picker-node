318ca89afe77b07377dfdf86aaa3540b6d127daf
Linh <klcr24@gmail.com>
2023-02-24 17:29:32 +0100

    Reform

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+require('dotenv').config();
+const express = require("express");
 const mongoose = require("mongoose");
 const Router = require("./routes")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(express.json());
+app.use(Router);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running at port 3000");
+});
+
+mongoose.connect(process.env.Connection_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
